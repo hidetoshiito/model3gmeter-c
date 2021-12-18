26ef3328ed0369d733480087afb2b19a4817f778
hidetoshi.ito <hidetoshi.ito03@gmail.com>
2021-12-18 23:40:25 +0900

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,7 +11,11 @@ module.exports = {
     parser: 'babel-eslint',
   },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // console, debuggerは書いて良い(build時に消す設定にしているので)
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'max-len': ['error', { code: 200 }],
+    //    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    //    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
 };
